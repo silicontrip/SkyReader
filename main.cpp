2e1c0bf6c2aa52d422c31e704cff1a1d3a2ae1ae
Mark Heath <mark@silicontrip.net>
2013-01-24 16:38:38 +1100

    Added getopt arguments, restructured program flow.

--- main.cpp
+++ main.cpp
@@ -15,27 +15,49 @@ void usage()
 {
 	printf("\n"
 		"Usage:\n"
-		"editor <option> <filename>\n"
+		"editor [-i <file>|-p] [-d] [-e] [-o <file>|-P] [-M <money>] [-X experience] ... \n"
 		"\n"
-		"<option>\n"
-		"r: read skylader data from portal, save unecrypted data to <filename>.\n"
-		"w: write skylader data to portal, read unencrypted <filename> and save\n"
-		"   to skylander.\n"
-		"u <optional skill path>: upgrade skylander stats.  <path> = - for left\n" 
-		"   skill path, + for right skill path, empty = do not upgrade skills.\n"
-		"   Original version saved as <filename>.\n"
-		   "d: dump the data of a skylander to the display.\n"
+		"Reading/Writing:\n"
+		"-i <file>\tread skylander data from file, with option to decrypt the data.\n"
+		   "-p\t\tread skylander data from portal and decrypt the data.\n"
+		   "-d\t\tdecrypt the data read from the file.\n"
+		"-o <file>\twrite skylander data to <filename>.\n"
+		   "-P\t\tencrypt and write skylander data to the portal.\n"
+		   "-e\t\tencrypt data when writing file.\n"
+		   "-D\t\tdump the data of a skylander to the display.\n"
+
+		   "\nUpgrade:\n"
+		   "-M <money>\tupgrade skylander money (max 65,000).\n" 		
+		   "-X <xp>\t\tupgrade skylander Experience (level 10 = 33,000).\n" 
+		   "-H <hp>\t\tupgrade skylander Hero Points (max 100).\n" 
+		   "-C <challenges>\tupgrade skylander challenges.\n"
+		   "-L <points>\tupgrade the skylander skillpoints on the left path.\n"
+		   "-R <points>\tupgrade the skylander skillpoints on the right path.\n"
+		   "-c\t\tupdate checksums.\n"
 		"\n"
 		"Examples: \n"
-		"editor u - dspyro.bak\n"
-		"This would save a copy of the figurine to the file dspyro.bak then upgrade\n"
-		"it using skills on the LEFT path seen in the character menu.\n"
+		"editor -p -o spyro.bin\n"
+		"This would save a copy of the figurine to the file dspyro.bak\n"
+		   "editor -i spyro.bin -o spyro_upgrade.bin -L 65535 -M 65000 -X 33000 -H 100\n"
+		"upgrade spyro.bin using skills on the LEFT path seen in the character menu\n"
+		   "and write it to file spyro_upgrade.bin\n"
 		"\n"
-		"editor u dspyro.bak\n"
-		"Upgrade skylander, leave skills as is.\n"
+		"editor -i spyro.bin -P -M 65000 -X 33000\n"
+		"Upgrade skylander, leave skills as is, and write to the portal.\n"
 		"\n"
-		"editor r dspyro.bak\n"
-		"Read figurine, decrypt and save to file dspyro.bak\n");
+		"editor -i spyro.bin -P\n"
+		"Read file from spyro.bin and write it to the portal.\n");
+}
+
+void printquad (unsigned int i)
+{
+
+	printf("%02X ",i & 0xff);
+	printf("%02X ",(i & 0xff00 )/ 0x100);
+	printf("%02X ",(i & 0xff0000) / 0x10000);
+	printf("%02X ",(i & 0xff000000) / 0x1000000);
+	printf("\n");
+	
 }
 
 void fprinthex(FILE *f, unsigned char *c, unsigned int n) {
@@ -72,32 +94,136 @@ int main(int argc, char* argv[])
 	unsigned char *buffer, *original_data;
 	bool OK, OK2;
 	
+	bool encrypt,decrypt,portalIn,portalOut,dump,upgrade;
+	
+	char * inFile, *outFile;
+	
+	const static char *legal_flags = "ePpcDo:i:dM:X:H:C:L:R:";
+
+	encrypt = false;
+	decrypt = false;
+	portalIn = false;
+	portalOut = false;
+	upgrade = false;
+	dump = false;
+	inFile = NULL;
+	outFile = NULL;
+	
+	unsigned int money, xp, hp, challenges, skillleft, skillright;
+	
+	money = 0;
+	xp = 0;
+	hp = 0;
+	challenges = 0;
+	skillleft = 0;
+	skillright = 0;
+	
 	SkylanderIO *skio;
 	Checksum crc;
 	
-	if (argc<2)
-	{
-		usage();
-		return 1;
+	int k;
+	
+	while ((k = getopt (argc, argv, legal_flags)) != -1) {
+		switch (k) {
+			case 'e': encrypt = true; break;
+			case 'd': decrypt = true; break;
+			case 'P': portalOut = true; break;
+			case 'p': portalIn = true; break;
+			case 'D': dump = true; break;
+			case 'i': 
+				inFile = new char[strlen(optarg)+1];
+				strcpy(inFile,optarg);
+				break;
+			case 'o':
+				outFile = new char[strlen(optarg)+1];
+				strcpy(outFile,optarg);
+				break;
+			case 'M':
+				money = atoi(optarg);
+				upgrade = true;
+				break;
+			case 'X':
+				xp = atoi(optarg);
+				upgrade = true;
+				break;
+			case 'H':
+				hp = atoi(optarg);
+				upgrade = true;
+				break;
+			case 'C':
+				challenges = atoi(optarg);
+				upgrade = true;
+				break;
+			case 'L':
+				skillleft = atoi(optarg);
+				upgrade = true;
+				break;
+			case 'R':
+				skillright = atoi(optarg);
+				upgrade = true;
+				break;
+		   case 'c':
+				upgrade = true;
+				break;
+			default:
+				usage () ;
+				exit (0);
+	
+		}
 	}
+
+	// validate command line options
+	if ( (!inFile && !portalIn) || (inFile && portalIn)) {
+		printf ("Must Choose One of: read from file -i <file> or read from portal -p\n");
+		usage();
+		exit(0);
+	}
+	
+	if (!outFile && !portalOut && !dump) {
+		printf ("Nothing to write. Choose file -o, portal -P or dump -D\n");
+		usage();
+		exit(0);
+	}	
+	
+	if (portalIn && upgrade) {
+		printf ("It is not recommended to upgrade directly from the portal (write to a file first)\n");
+		usage();
+		exit(0);
+	}
+	
+	if (portalIn && portalOut) {
+		printf ("It is not recommended to read and write directly from the portal (write to a file first)\n");
+		usage();
+		exit(0);
+	}
+	
+	
+	
 	
 	try {
 	
-	switch(*argv[1]) {
-		case 'd':
-			
-			if(argc !=3) {
-				usage();
-				return 1;
-			}			
-			
-			skio = new SkylanderIO();
-			
-			skio->initWithUnencryptedFile(argv[2]);
-			if(! skio->getSkylander()->validateChecksum()) {
-				fprintf(stderr, "Warning. Skylander data read from portal, but checksums are incorrect.  File may be corrupt.\n");
+		skio = new SkylanderIO();
+		
+		printf ("Reading Skylander\n");
+		
+		if (portalIn) {
+			skio->initWithPortal();
+		}
+		if (inFile) {
+			if (decrypt) {
+				skio->initWithEncryptedFile(inFile);
+			} else {
+				skio->initWithUnencryptedFile(inFile);
 			}
-
+		}
+		
+		if(! skio->getSkylander()->validateChecksum()) {
+			fprintf(stderr, "Warning. Skylander data read from portal, but checksums are incorrect.  File may be corrupt.\n");
+		} else {
+			printf ("Skylander Checksum OK.\n");
+		}
+		
+		if (dump) {
 			Skylander * sky ;
 			sky = skio->getSkylander() ;
 			
@@ -121,102 +247,39 @@ int main(int argc, char* argv[])
 			printf("Hero Points: %d\n",sky->getHeroPoints());
 
 			printf("Heroic Challenges: %x\n",sky->getHeroicChallenges());
+		}
+	
+		if (upgrade) {
+			if (money) { skio->getSkylander()->setMoney(money); }
+			if (xp) { skio->getSkylander()->setXP(xp); }
+			if (hp) { skio->getSkylander()->setHeroPoints(hp); }
+			if (challenges) { skio->getSkylander()->setHeroicChallenges(challenges); }
+			if (skillleft) { skio->getSkylander() ->setSkillLeft(skillleft); }
+			if (skillright) { skio->getSkylander() ->setSkillLeft(skillright); }
 			
-			delete skio;
-			
-			break;
-		case 'r':
-			// read from portal
-
-			if(argc !=3) {
-				usage();
-				return 1;
-			}
-			
-			skio = new SkylanderIO();
-
-			skio->initWithPortal();
-
-			// DecryptBuffer(buffer);
-			if(! skio->getSkylander()->validateChecksum()) {
-				fprintf(stderr, "Warning. Skylander data read from portal, but checksums are incorrect.  File may be corrupt.\n");
-			}
-			
-			skio->writeSkylanderToUnencryptedFile(argv[2]);
-			
-			delete skio;
-			break;
-
-		case 'w':
-			// write to portal
-
-			if(argc !=3) {
-				usage();
-				return 1;
-			}
-
-			skio = new SkylanderIO();
-			skio->initWithUnencryptedFile(argv[2]);
 			skio->getSkylander()->computeChecksum();
-			
-			skio->writeSkylanderToPortal();
-			
-			delete skio;
-			break;
-
-		case 'u':
-			// update 
-			if(argc !=4  && argc != 3) {
-				usage();
-				return 1;
+		}
+		
+			if (outFile || portalOut)  {
+				printf ("Writing Skylander.\n");
 			}
-
-			char *filename;
-			char skill_path;
-
-			if(argc == 4) {
-				skill_path = *argv[2];
-				filename = argv[3];
-				if(skill_path != '-' && skill_path != '+') {
-					fprintf(stderr, "Error. When you run the upgrade command you must select an upgrade path. "
-						"- is left, + is right, <empty> is leave skills unchanged.\n");
-					usage();
-					return false;
-				}
+		if (outFile) {
+			if (encrypt) {
+				skio->writeSkylanderToEncryptedFile(outFile);
 			} else {
-				skill_path = 0;
-				filename = argv[2];
+				skio->writeSkylanderToUnencryptedFile(outFile);
 			}
-			
-			skio = new SkylanderIO();
-			
-			skio -> initWithPortal();
-			
-			if(! skio->getSkylander()->validateChecksum()) {
-				fprintf(stderr, "Warning. Skylander data read from portal, but checksums are incorrect.  File may be corrupt.\n");
-				return 1;
-			}
-			
-			skio->writeSkylanderToUnencryptedFile(filename);
-
-			if (skill_path == '-')
-				skio->getSkylander()->MaxSkills(0xFF);
-			else 
-				skio->getSkylander()->MaxSkills(0xFD);
-
-			skio->getSkylander()->computeChecksum();
+		}
+		if (portalOut)
+		{
 			skio->writeSkylanderToPortal();
+		}
+		
+		delete skio;
 			
-			delete skio;
-			
-			break;
-		default:
-			usage();
-			return 1;
-	}
 
-	printf("Success!\n");
-	return 0;
+		printf("\nSuccess!\n");
+		return 0;
 		
 	} catch (int e) {
 	
